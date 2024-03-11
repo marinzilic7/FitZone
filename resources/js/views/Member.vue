60818833fb68c7296c92622a167bdc8c28c90257
marinzilic7 <marinzilic507@gmail.com>
2024-03-11 17:26:51 +0100

    update

--- resources/js/views/Member.vue
+++ resources/js/views/Member.vue
@@ -169,6 +169,7 @@ export default {
         this.getUser();
         this.getCoaches();
         this.getTraining();
+        this.checkLoginStatus();
     },
     mounted() {
         this.fetchCsrfToken();
