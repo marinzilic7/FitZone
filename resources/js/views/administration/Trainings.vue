82fc00355f3fce92f0836fa7b8ff7e38f44ff182
marinzilic7 <marinzilic507@gmail.com>
2024-03-11 17:10:35 +0100

    update

--- resources/js/views/administration/Trainings.vue
+++ resources/js/views/administration/Trainings.vue
@@ -103,7 +103,7 @@ import Modal from "../../components/Modal.vue";
                                 <label
                                     for="recipient-name"
                                     class="col-form-label text-light"
-                                    >Cijena treninga mjesecno:</label
+                                    >Cijena treninga:</label
                                 >
                                 <input
                                     type="text"
